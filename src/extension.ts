f14a9d4ee698492c16bbfe4dcf86ab09f0973e68
morehardy <morehardy@163.com>
2018-12-23 17:25:14 +0800

    feat: register

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,39 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import CSSPercent from './process';
+import CSSPercentProvider from './provider';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let config;
+
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "css-percent" is now active!');
+    config = vscode.workspace.getConfiguration('CSSPercent');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    const process = new CSSPercent(config);
+    const provider = new CSSPercentProvider(process);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    const TYPES = [
+        'html',
+        'vue',
+        'css',
+        'less',
+        'scss',
+        'sass',
+        'stylus'
+    ];
+
+    TYPES.forEach(item => {
+        let providerDisposable = vscode.languages.registerCompletionItemProvider(
+            {
+                scheme: 'file',
+                language: item
+            },
+            provider,
+            'w',
+            'h'
+        );
+        context.subscriptions.push(providerDisposable);
     });
-
-    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
